69256f9ecbd5f10ed9b7f329dc4f38cbd9250e98
Adam Calkin <calkinadam@gmail.com>
2019-04-18 11:06:09 +1200

    add start react

---
+++ client/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Body from './Body'
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Development</h1>
+      <Body />
+    </div>
+  )
+}
+
+export default App
+
--- index.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app')
-
-
-const port = process.env.PORT || 3000
-
-app.listen(port, function () {
-  console.log('Server is listening on port', port)
-})
